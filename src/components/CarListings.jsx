7ce23c0082a542fbf0423f929c561d537cd79204
Edwin Murimi <edwinfmaina19@gmail.com>
2021-08-23 13:07:59 +0300

    started on car-listings.jsx and modified body-carousel.jsx

--- src/components/CarListings.jsx
+++ src/components/CarListings.jsx
@@ -1,22 +1,124 @@
 import React from 'react'
+import '../App.css'
+import '../styling/CarListings.css'
 
 function CarListings() {
   return (
-    <div>
-      <span><hr/>Car listings<hr/></span>
-      <div className="car-listings-container">
-        <div className="car-listing-item">
-          <div className="">
-            <img src="" alt="" />
-            <span></span>
-          </div>
-          <div className="car-listing-description">
-            <div>
-              <span></span>
-              <br/>
-              <span></span>
+    <div className="listings-container">
+      <div className="container">
+        <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}><hr style={{ width: '300px', marginRight: '30px' }}/><span className="car-listings">Car listings</span><hr style={{ width: '300px', marginLeft: '30px' }} /></span>
+        <div className="car-listings-container">
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
             </div>
-            <button></button>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
+          </div>
+          <div className="car-listing-item">
+            <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
+              <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
+              <span style={{ position: 'absolute', left: 0, top: 0 }} >Subaru Forester 2014 model</span>
+            </div>
+            <div className="car-listing-description">
+              <div className="car-listings-inner-description">
+                <span></span>
+                <br/>
+                <span></span>
+              </div>
+              <button></button>
+            </div>
           </div>
         </div>
       </div>
