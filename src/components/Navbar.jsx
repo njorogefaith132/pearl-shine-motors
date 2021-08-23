a68332e485b9f7ba317106429360e7b1898dd644
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-08-23 10:20:09 +0300

    navbar +styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className = "navbar">
+            <div className="container navbar-container">
+                <h1>Pearl Shine Motors</h1>
+
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Navbar
