643ada646554f6364c46d594569719556188de1a
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-08-23 13:31:42 +0300

    car listings heading

--- src/components/CarListings.jsx
+++ src/components/CarListings.jsx
@@ -6,7 +6,7 @@ function CarListings() {
   return (
     <div className="listings-container">
       <div className="container">
-        <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}><hr style={{ width: '300px', marginRight: '30px' }}/><span className="car-listings">Car listings</span><hr style={{ width: '300px', marginLeft: '30px' }} /></span>
+        <span className="listings-heading"><hr/><h4>Car listings</h4><hr/></span>
         <div className="car-listings-container">
           <div className="car-listing-item">
             <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
@@ -22,6 +22,7 @@ function CarListings() {
               <button></button>
             </div>
           </div>
+
           <div className="car-listing-item">
             <div style={{ position: 'relative', backgroundColor: 'red', width: '300px', height: '300px' }} className="car-listings-image">
               <img style={{ width: '300px' }} src="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="car in a desert" />
