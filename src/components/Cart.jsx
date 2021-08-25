519cb39d212e4f74b57266c2e4d8dc3f0c872626
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-08-25 13:57:47 +0300

    added cart.jsx and cart.css

---
+++ src/components/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styling/Cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            
+
+            
+
+            
+        </div>
+    )
+}
+
+export default Cart
